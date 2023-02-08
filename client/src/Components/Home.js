af058c49692727b998330f651eae7928bf1f1df5
Riadh18 <allaniriadh103@gmail.com>
2023-02-08 09:09:03 -1100

    first commit

---
+++ client/src/Components/Home.js
@@ -0,0 +1,10 @@
+const Home=()=>{
+    return(
+        <div>
+            <h2>Home</h2>
+            <img src="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2018_30/1355945/home-exterior-today-180726-tease.jpg" width="500" height="500"></img>
+        </div>
+    )
+}
+
+export default Home
